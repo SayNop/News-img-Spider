3d532686c26abc2b1049ce660679e5606e479363
Yangchen <a2045751@163.com>
2021-03-09 02:15:22 +0800

    Xpath

--- News_img/News_img/spiders/img.py
+++ News_img/News_img/spiders/img.py
@@ -3,8 +3,13 @@ import scrapy
 
 class ImgSpider(scrapy.Spider):
     name = 'img'
+    # 2.检测域名范围
     allowed_domains = ['hnr.cn']
+    # 1.修改起始的url
     start_urls = ['http://hnr.cn/']
 
     def parse(self, response):
-        pass
+        # 提取数据
+        # 获取图片节点
+        nodes = response.xpath('//div[@class="TRS_Editor"]/p[1]/font/img')
+        # 翻页
